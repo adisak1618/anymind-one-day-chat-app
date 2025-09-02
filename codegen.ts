41066178afe272d48fa22ea92214f81309a37bba
oat-taobin <adisak.c@tao-bin.com>
2025-09-02 22:55:20 +0700

    feat: setup graphql-codegen

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://angular-test-backend-yc4c5cvnnq-an.a.run.app/graphql",
+  documents: "src/gql/**/*.graphql",
+  generates: {
+    "src/gql/generated/": {
+      preset: "client",
+      plugins: [],
+      config: {
+        useTypeImports: true
+      },
+    }
+  }
+};
+
+export default config;
