2dd885fedb7b9b3852f4aefee5f113ea1ce05623
oat-taobin <adisak.c@tao-bin.com>
2025-09-02 23:56:02 +0700

    feat: integrate Apollo Client for GraphQL data management

---
+++ src/gql/client.ts
@@ -0,0 +1,14 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+
+const httpLink = new HttpLink({
+  uri: 'https://angular-test-backend-yc4c5cvnnq-an.a.run.app/graphql',
+})
+
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+export default apolloClient
+
+
