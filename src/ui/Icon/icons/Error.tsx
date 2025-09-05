7fcd7079d3c12be29752a7e40ff3e73f1b9e1a6e
oat-taobin <adisak.c@tao-bin.com>
2025-09-06 02:41:38 +0700

    feat: add ChatBubble Icon

--- src/ui/Icon/icons/Error.tsx
+++ src/ui/Icon/icons/Error.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import type { IconProps } from "../type";
 
 export function ErrorIcon({
-  color = "currentcolor",
   className,
 }: IconProps): React.JSX.Element {
   return (
@@ -18,7 +17,7 @@ export function ErrorIcon({
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-1.5-5.009c0-.867.659-1.491 1.491-1.491.85 0 1.509.624 1.509 1.491 0 .867-.659 1.509-1.509 1.509-.832 0-1.491-.642-1.491-1.509zM11.172 6a.5.5 0 0 0-.499.522l.306 7a.5.5 0 0 0 .5.478h1.043a.5.5 0 0 0 .5-.478l.305-7a.5.5 0 0 0-.5-.522h-1.655z"
-        fill={color}
+        fill="currentcolor"
       />
     </svg>
   );
--- src/ui/Icon/type.ts
+++ src/ui/Icon/type.ts
@@ -1,4 +1,3 @@
 export type IconProps = {
-  color?: string;
   className?: string;
 }
