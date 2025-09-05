2ca7e9703627ad58a25b08ce01a17a92ed84e04e
oat-taobin <adisak.c@tao-bin.com>
2025-09-06 02:43:58 +0700

    feat: add SendIcon and update Icon exports to include new icons

--- src/ui/Icon/index.tsx
+++ src/ui/Icon/index.tsx
@@ -1,3 +1,5 @@
 export { ErrorIcon } from "./icons/Error";
 export { LoadingIcon } from "./icons/Loading";
 export { SuccessIcon } from "./icons/Success";
+export { SendIcon } from "./icons/Send";
+export { ChatBubbleIcon } from "./icons/ChatBubble";
