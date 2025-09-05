c4dab857e814b0f820025f895b985288c296cc14
oat-taobin <adisak.c@tao-bin.com>
2025-09-05 23:17:22 +0700

    feat: enhance Message component to include status indicators and datetime display

--- src/modules/chat/components/Message.tsx
+++ src/modules/chat/components/Message.tsx
@@ -1,17 +1,24 @@
 import type { UserId } from "@/gql/generated/graphql";
 import Avatar from "@/modules/user/components/Avatar";
 import { cn } from "@/lib/cn";
+import { ErrorIcon, LoadingIcon, SuccessIcon } from "@/ui/Icon";
+import clsx from "clsx";
+import { formatDate } from "date-fns";
 
 type MessageProps = {
   userId: UserId;
   text: string;
   isMe: boolean;
   messageId: string;
+  status: "loading" | "success" | "error";
+  datetime: Date;
+  onResend?: () => void;
+  onUndoSend?: () => void;
 };
 
-export const Message = ({ userId, text, isMe, messageId }: MessageProps) => {
+export const Message = ({ userId, text, isMe, status, messageId, datetime, onResend, onUndoSend }: MessageProps) => {
   return (
-    <div className={cn("flex items-end gap-2 mb-4", isMe ? "flex-row-reverse" : "flex-row")}>
+    <div key={messageId} className={cn("flex items-end gap-2", isMe ? "flex-row-reverse" : "flex-row")}>
       <Avatar userId={userId} />
       <div className="relative max-w-xs lg:max-w-md">
         {/* Chat bubble */}
@@ -36,7 +43,21 @@ export const Message = ({ userId, text, isMe, messageId }: MessageProps) => {
           )}
         />
       </div>
-      <p className="text-xs text-gray-500">{messageId === "temp-id" ? "sending..." : "sended"}</p>
+      <MessageStatus status={status} />
+      <p className="text-xs text-gray-500">{formatDate(datetime, "HH:mm")}</p>      
+      {status === "error" && onUndoSend && <p className="text-xs text-red-500 cursor-pointer hover:underline hover:font-medium" onClick={onUndoSend}>Undo Send</p>}
+      {status === "error" && onResend && <p className="text-xs text-blue-500 cursor-pointer hover:underline hover:font-medium" onClick={onResend}>Resend</p>}
     </div>
   );
 };
+
+function MessageStatus({ status }: Pick<MessageProps, "status">) {
+  const baseClassName = "w-4 h-4";
+  return (
+    <div>
+      {status === "loading" && <LoadingIcon className={clsx(baseClassName, "text-blue-500 animate-spin")} />}
+      {status === "success" && <SuccessIcon className={clsx(baseClassName, "text-green-500")} />}
+      {status === "error" && <ErrorIcon className={clsx(baseClassName, "text-red-500")} />}
+    </div>
+  )
+}
