4423b00a427fa0f7e6ed933766bfde49751b95a9
oat-taobin <adisak.c@tao-bin.com>
2025-09-06 00:17:57 +0700

    feat: create useSendMessageHook for handling message sending and cache updates in chat

---
+++ src/modules/chat/api/hooks/useSendMessageHook.ts
@@ -0,0 +1,62 @@
+import { ChannelId, MessagesFetchLatestDocument, UserId, type MessagePostMutation, type MessagePostMutationVariables, type MessagesFetchLatestQuery } from "@/gql/generated/graphql";
+import { useMessagePostMutation } from "./useMessagePostMutation";
+import type { useMutation } from "@apollo/client/react";
+
+type UseSendMessageHookProps = {
+  channelId: ChannelId;
+  onCompleted?: useMutation.Options<MessagePostMutation, MessagePostMutationVariables>["onCompleted"];
+  onError?: useMutation.Options<MessagePostMutation, MessagePostMutationVariables>["onError"];
+};
+
+type sendMessageProps = {
+  text: string;
+  userId: UserId;
+};
+
+export const useSendMessageHook = ({ channelId, onCompleted, onError }: UseSendMessageHookProps) => {
+  const [mutation] = useMessagePostMutation({
+    update: (cache, data) => {
+      const cachedData = cache.readQuery<MessagesFetchLatestQuery>({
+        query: MessagesFetchLatestDocument,
+        variables: { channelId },
+      });
+
+      if (!data?.data?.MessagePost) return;
+
+      cache.writeQuery({
+        query: MessagesFetchLatestDocument,
+        variables: { channelId },
+        data: {
+          MessagesFetchLatest: [
+            ...(cachedData?.MessagesFetchLatest ?? []),
+            data?.data?.MessagePost,
+          ],
+        },
+      });
+    },
+  });
+
+  const sendMessage = ({ text, userId }: sendMessageProps) => {
+    mutation({
+      variables: {
+        channelId,
+        userId,
+        text,
+      },
+      optimisticResponse: {
+        MessagePost: {
+          __typename: "MessageEnum",
+          userId: UserId.Sam,
+          messageId: "temp-id",
+          text,
+          datetime: new Date().toISOString(),
+        },
+      },
+      onCompleted,
+      onError,
+    });
+  };
+
+
+  return { sendMessage }
+};
