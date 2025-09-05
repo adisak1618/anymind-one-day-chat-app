bb22133cca253baab5c0c2bfc77a05af8d4d6c9a
oat-taobin <adisak.c@tao-bin.com>
2025-09-06 00:18:03 +0700

    refactor: replace useMessagePostMutation with useSendMessageHook in ChatPanel for improved message handling

--- src/pages/chat/_components/ChatPanel/index.tsx
+++ src/pages/chat/_components/ChatPanel/index.tsx
@@ -1,14 +1,12 @@
 import { useRef, useState } from "react";
 import { Composer } from "@/modules/chat/components/Composer";
-import { useMessagePostMutation } from "@/modules/chat/api/hooks/useMessagePostMutation";
 import {
   ChannelId,
-  MessagesFetchLatestDocument,
   UserId,
-  type MessagesFetchLatestQuery,
 } from "@gql/generated/graphql";
 import { MessageListContainer } from "../MessageListContainer";
 import type { ErrorMessageType } from "./type";
+import { useSendMessageHook } from "@/modules/chat/api/hooks/useSendMessageHook";
 
 type ChatPanelProps = {
   channelId: ChannelId;
@@ -21,40 +19,41 @@ type ChannelMessagesType = {
 const ChatPanel = ({ channelId }: ChatPanelProps) => {
   const messageListRef = useRef<HTMLDivElement>(null);
   const [selectedUserId, setSelectedUserId] = useState<UserId>(UserId.Sam);
-  const [errorMessages, setErrorMessage] = useState<ErrorMessageType[]>([{
-    id: crypto.randomUUID(),
-    channelId: ChannelId.Lgtm,
-    message: "test",
-    userId: selectedUserId,
-    datetime: new Date().toISOString(),
-  }]);
-  
+  const [errorMessages, setErrorMessage] = useState<ErrorMessageType[]>([
+    {
+      id: crypto.randomUUID(),
+      channelId: ChannelId.Lgtm,
+      message: "test",
+      userId: selectedUserId,
+      datetime: new Date().toISOString(),
+    },
+  ]);
 
   const [message, setMessage] = useState<ChannelMessagesType>({});
-  const [postMessage] = useMessagePostMutation({
-    update: (cache, data) => {
-      const cachedData = cache.readQuery<MessagesFetchLatestQuery>({
-        query: MessagesFetchLatestDocument,
-        variables: { channelId },
+  const { sendMessage } = useSendMessageHook({
+    channelId,
+    onCompleted: () => {
+      setMessage({
+        ...message,
+        [channelId]: "",
       });
-
-      if (!data?.data?.MessagePost) return;
-
-      cache.writeQuery({
-        query: MessagesFetchLatestDocument,
-        variables: { channelId },
-        data: {
-          MessagesFetchLatest: [
-            ...(cachedData?.MessagesFetchLatest ?? []),
-            data?.data?.MessagePost,
-          ],
+    },
+    onError: (_, context) => {
+      setErrorMessage([
+        ...errorMessages,
+        {
+          id: crypto.randomUUID(),
+          channelId,
+          message: context?.variables?.text ?? "",
+          userId: selectedUserId,
+          datetime: new Date().toISOString(),
         },
-      });
+      ]);
     },
   });
 
   const scrollToBottom = (behavior: ScrollBehavior) => {
-    if(messageListRef.current) {
+    if (messageListRef.current) {
       messageListRef.current.scrollTo({
         top: messageListRef.current.scrollHeight,
         behavior: behavior,
@@ -70,46 +69,17 @@ const ChatPanel = ({ channelId }: ChatPanelProps) => {
     const text = message[channelId].trim();
     if (!text) return;
     scrollToBottom("smooth");
-
-    postMessage({
-      variables: {
-        channelId,
-        userId: selectedUserId,
-        text,
-      },
-      optimisticResponse: {
-        MessagePost: {
-          __typename: "MessageEnum",
-          userId: UserId.Sam,
-          messageId: "temp-id",
-          text,
-          datetime: new Date().toISOString(),
-        },
-      },
-      onCompleted: () => {
-        setMessage({
-          ...message,
-          [channelId]: "",
-        });
-      },
-      onError: (_, context) => {
-        setErrorMessage([...errorMessages, {
-          id: crypto.randomUUID(),
-          channelId,
-          message: context?.variables?.text ?? "",
-          userId: selectedUserId,
-          datetime: new Date().toISOString(),
-        }]);
-      },
-    });
+    sendMessage({ text, userId: selectedUserId });
   };
 
   return (
     <div className="flex flex-col gap-4 h-full">
-      <div ref={messageListRef}  className="flex flex-col flex-1 overflow-auto rounded-md border border-gray-200 p-3 bg-white">
-
-        <MessageListContainer 
-          channelId={channelId} 
+      <div
+        ref={messageListRef}
+        className="flex flex-col flex-1 overflow-auto rounded-md border border-gray-200 p-3 bg-white"
+      >
+        <MessageListContainer
+          channelId={channelId}
           selectedUserId={selectedUserId}
           onMessagesLoaded={handleMessagesLoaded}
           errorMessages={errorMessages}
